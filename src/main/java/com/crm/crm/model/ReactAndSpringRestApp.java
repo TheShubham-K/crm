378177016e02b442e97cb85271b03b8ffa6c9c4b
Subham Kumar <subhamk@juniper.net>
2022-04-11 12:57:52 +0530

    crm

---
+++ src/main/java/com/crm/crm/model/ReactAndSpringRestApp.java
@@ -0,0 +1,13 @@
+package com.crm.crm.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactAndSpringRestApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactAndSpringRestApp.class, args);
+    }
+    
+}
